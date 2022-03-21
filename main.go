23c84cbbaac1190c5d156bc93de2d48dc9a658f5
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2022-03-21 18:55:37 +0300

    satisfy linter

--- main.go
+++ main.go
@@ -289,6 +289,9 @@ func addText(arr []string, str string) []string {
 func getPage(pageUrl string) []byte {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", pageUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 	req.Header.Add("User-Agent", `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.27 Safari/537.36`)
 	res, err := client.Do(req)
 	if err != nil {
